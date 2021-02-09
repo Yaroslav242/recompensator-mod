3447ae23cdcb894c6b9c92654315d22d208b85e9
Yaroslav242 <Yaroslav.Polosin@gmail.com>
2021-02-09 11:14:12 +0000

    Revert "Add files via upload"
    This reverts commit 6a3b0fc41d04a1f738965800fd3b68eadd83a226.

--- mod.js
+++
@@ -1,5 +0,0 @@
-var mod = Vars.mods.locateMod("recompensator-mod");
-var x = mod.meta;
-x.displayName = "[#33ff77]The Recompensator weapon mod";
-x.description = "[#f2ed5e]Adds the recompensator, an electrically-powered liquid capsule launcher! Warning: launch [#eb4034]BIG THICK HOT LAVA at enemies with extreme prejudice";
-x.author = "[#33ff77]Yarik";
